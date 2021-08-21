1ecf1a525d68fe6eb2ae558fedd3a7b7ebe1b6f8
nuradil <696.overlord.696@gmail.com>
2021-08-22 00:00:26 +0600

    polling

--- main.py
+++ main.py
@@ -446,3 +446,5 @@ def post_message(message):
 
 if __name__ == '__main__':
     server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    bot.remove_webhook()
+    bot.polling(none_stop=True)
